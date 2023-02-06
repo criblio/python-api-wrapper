4698695bd1c6396c31a08fd168b36903a2b4f659
Rob Franz <rfranz@cribl.io>
2023-02-06 16:15:05 -0600

    Update collector_operations.py
    add get_collection_jobs

--- cribl_python_api_wrapper/collectors/collector_operations.py
+++ cribl_python_api_wrapper/collectors/collector_operations.py
@@ -19,11 +19,27 @@ def get_collectors(base_url, cribl_auth_token, worker_group=None):
             "General exception raised while attempting to get collector information from Cribl: %s" % str(e))
 
 
+def get_collection_jobs(base_url, cribl_auth_token, worker_group=None):
+    headers = {"Content-type": "application/json",
+               "Authorization": "Bearer " + cribl_auth_token}
+    payload = None
+
+    try:
+        if worker_group is not None:
+            return get(base_url + "/m/" + worker_group + "/lib/jobs", headers=headers, payload=payload)
+        else:
+            return get(base_url + "/lib/jobs", headers=headers, payload=payload)
+
+    except Exception as e:
+        raise Exception(
+            "General exception raised while attempting to get collection job information from Cribl: %s" % str(e))
+
+
 def get_collector_by_id(base_url, cribl_auth_token, collector_id, worker_group=None):
     headers = {"Content-type": "application/json",
                "Authorization": "Bearer " + cribl_auth_token}
 
-    payload = collector = None
+    payload = None
 
     try:
         if worker_group is not None:
