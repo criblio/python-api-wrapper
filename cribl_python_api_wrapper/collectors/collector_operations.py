7cfb22c52a2722f73211dd57c46a640d9ca89410
Rob Franz <rfranz@cribl.io>
2023-06-16 12:42:41 -0500

    added create_collector

--- cribl_python_api_wrapper/collectors/collector_operations.py
+++ cribl_python_api_wrapper/collectors/collector_operations.py
@@ -52,3 +52,22 @@ def get_collector_by_id(base_url, cribl_auth_token, collector_id, worker_group=N
     except Exception as e:
         raise Exception(
             "General exception raised while attempting to get collector information from Cribl: %s" % str(e))
+
+
+def create_collector(base_url, cribl_auth_token, create_config, worker_group=None):
+    headers = {"Content-type": "application/json",
+               "Authorization": "Bearer " + cribl_auth_token}
+
+    payload = create_config
+
+    try:
+        if worker_group is not None:
+            return post(base_url + "/m/" + worker_group + "/lib/jobs",
+                        headers=headers, payload=payload)
+        else:
+            return get(base_url + "/lib/jobs",
+                       headers=headers, payload=payload)
+
+    except Exception as e:
+        raise Exception(
+            "General exception raised while attempting to get collector information from Cribl: %s" % str(e))
