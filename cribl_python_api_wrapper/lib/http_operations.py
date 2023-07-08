46b686afab4d16c8bf726b3c082e59ba3e31c004
Rob Franz <rfranz@cribl.io>
2023-07-07 21:25:27 -0400

    support for enabling/disabling SSL verification
    - e.g. if self-signed certs are in use, this will allow requests library to skip SSL verification

--- cribl_python_api_wrapper/lib/http_operations.py
+++ cribl_python_api_wrapper/lib/http_operations.py
@@ -5,29 +5,28 @@ import json
 # wrapper functions around requests methods
 # returns Response object
 
-
-def get(url, headers, payload, stream=False):
+def get(url, headers, payload, stream=False, verify=True):
     if payload is not None:
-        return requests.get(url, data=json.dumps(payload), headers=headers, stream=stream)
+        return requests.get(url, data=json.dumps(payload), headers=headers, stream=stream, verify=verify)
     else:
-        return requests.get(url, headers=headers, stream=stream)
+        return requests.get(url, headers=headers, stream=stream, verify=verify)
 
 
-def post(url, headers, payload):
-    return requests.post(url, data=json.dumps(payload), headers=headers)
+def post(url, headers, payload, verify=True):
+    return requests.post(url, data=json.dumps(payload), headers=headers, verify=verify)
 
 
 # should be used for file uploads (e.g. lookup files, packs)
-def put(url, headers, data):
-    return requests.put(url, headers=headers, data=data)
+def put(url, headers, data, verify=True):
+    return requests.put(url, headers=headers, data=data, verify=verify)
 
 
-def patch(url, headers, payload):
-    return requests.patch(url, data=json.dumps(payload), headers=headers)
+def patch(url, headers, payload, verify=True):
+    return requests.patch(url, data=json.dumps(payload), headers=headers, verify=verify)
 
 
-def delete(url, headers, payload=None):
+def delete(url, headers, payload=None, verify=True):
     if payload is not None:
-        return requests.delete(url, headers=headers, data=json.dumps(payload))
+        return requests.delete(url, headers=headers, verify=verify, data=json.dumps(payload))
     else:
-        return requests.delete(url, headers=headers)
+        return requests.delete(url, headers=headers, verify=verify)
